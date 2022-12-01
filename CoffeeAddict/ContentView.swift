8d6a8d94db03f5e61bfb4d712ccb9c349d747ef2
Zalfa <zalfa.muhammad@gmail.com>
2022-12-01 22:38:51 +0700

    implement tab bar

--- CoffeeAddict/ContentView.swift
+++ CoffeeAddict/ContentView.swift
@@ -8,13 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: PROPERTIES
+    @State private var selectedTab: Tabs = .browse
+    
+    // MARK: BODY
     var body: some View {
-        CoffeeMainView()
+        VStack {
+            TabView(selection: $selectedTab) {
+                CoffeeMainView()
+                    .listStyle(.plain)
+//                Text("browse".uppercased())
+//                    .font(.system(.title, design: .rounded))
+                    .tabItem {
+                        Image(systemName: "square.grid.2x2")
+                        Text("Browse")
+                    }
+                    .tag(Tabs.browse)
+                VStack {
+                    Text("watch".uppercased())
+                        .font(.system(.title, design: .rounded))
+                    Button {
+                        selectedTab = Tabs.profile
+                    } label: {
+                        Text("Show Profile")
+                            .font(.system(.headline, design: .rounded))
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(.black)
+                            .cornerRadius(10)
+                            .padding()
+                    }
+
+                } // MARK: VStack
+                .tabItem {
+                    Image(systemName: "play.rectangle")
+                    Text("Watch")
+                }
+                .tag(Tabs.watch)
+                Text("loans".uppercased())
+                    .font(.system(.title, design: .rounded))
+                    .tabItem {
+                        Image(systemName: "rectangle.and.text.magnifyingglass")
+                        Text("Loans")
+                    }
+                    .tag(Tabs.loans)
+                Text("profile".uppercased())
+                    .font(.system(.title, design: .rounded))
+                    .tabItem {
+                        Image(systemName: "person")
+                        Text("Profile")
+                    }
+                    .tag(Tabs.profile)
+            } // MARK: TABVIEW
+            .tint(.yellow)
+        }
+        
     }
 }
 
+// MARK: PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+enum Tabs: String {
+    case browse
+    case watch
+    case loans
+    case profile
 }
